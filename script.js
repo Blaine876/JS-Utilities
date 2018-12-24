c90f19e64be073c17f507d4e63ac44687e444dfb
Blaine Oakley <blaine.oakley@gmail.com>
2018-12-24 05:45:37 -0500

    working tip calculator

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
-function tipCalc()
+
+/********************************************************************************************
+ * 
+ *                                TIP CALCULATOR FUNCTIONS
+ *                                 @function tipTen 
+ *                                @function tipFifteen 
+ *                                 @function tipTwenty
+ *                                 @function tipCustom
+ **********************************************************************************************/
+
+function tipTen()
 {
-    var amount = document.getElementById('bill').value;
-    var percent = document.getElementById('ten').value;
-    var tip = amount * (percent/100);
-    return tip;
+    var amount = parseInt ( document.getElementById("bill").value ) ;
+    var percent = parseInt ( document.getElementById("ten").value ) ;
+    const tip = amount * percent / 100;
+    document.getElementById("tip").innerHTML = "Tip: $" + tip;
+}
+
+function tipFifteen()
+{
+    var amount = parseInt ( document.getElementById("bill").value ) ;
+    var percent = parseInt ( document.getElementById("fifteen").value ) ;
+    const tip = amount * percent / 100;
+    document.getElementById("tip").innerHTML = "Tip: $" + tip;
+}
+
+function tipTwenty()
+{
+    var amount = parseInt ( document.getElementById("bill").value ) ;
+    var percent = parseInt ( document.getElementById("twenty").value ) ;
+    const tip = amount * percent / 100;
+    document.getElementById("tip").innerHTML = "Tip: $" + tip;
+}
+
+function tipCustom()
+{
+    var amount = parseInt ( document.getElementById("bill").value ) ;
+    var percent = parseInt ( document.getElementById("other").value ) ;
+    const tip = amount * percent / 100;
+    document.getElementById("tip").innerHTML = "Tip: $" + tip;
 }
