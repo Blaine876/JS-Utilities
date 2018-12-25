505422cb5ba2e18fbdeb6c7216f4c075f5805573
Blaine Oakley <blaine.oakley@gmail.com>
2018-12-25 01:07:00 -0500

    added metric converter and started todo list

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 
 /********************************************************************************************
  * 
- *                                TIP CALCULATOR FUNCTIONS
- *                                 @function tipTen 
+ *                                TIP CALCULATOR 
+ *                                FUNCTIONS
+ *                                @function tipTen 
  *                                @function tipFifteen 
- *                                 @function tipTwenty
- *                                 @function tipCustom
+ *                                @function tipTwenty
+ *                                @function tipCustom
  **********************************************************************************************/
 
 function tipTen()
@@ -39,3 +40,106 @@ function tipCustom()
     const tip = amount * percent / 100;
     document.getElementById("tip").innerHTML = "Tip: $" + tip;
 }
+
+/********************************************************************************* */
+/*
+ *                              UNIT CONVERSION
+ *                               FUNCTIONS
+ *                               @function unitCOnvert 
+ *  3 feet = 1 yard
+ *  100 yard = 1 mile
+ *  
+ ******************************************************************************************/
+
+ function unitConvert()
+ {
+    var myInput = parseInt(document.getElementById("firstUnit").value);
+    var first_unit = document.getElementById("mySelect1").value;
+    var second_unit = document.getElementById("mySelect2").value;
+    
+    // MILLIMETER CONVERSIONS
+    if (first_unit == "mm" && second_unit == "mm")
+    {
+        result = myInput;
+    }
+
+    else if (first_unit == "mm" && second_unit == "cm")
+    {
+        result = myInput * 10;
+    }
+
+    else if (first_unit == "mm" && second_unit == "m")
+    {
+        result = myInput * 1000;
+    }
+
+    else if (first_unit == "mm" && second_unit == "km")
+    {
+        result = myInput * 10000;
+    }
+
+    // CENTIMETER CONVERSIONS
+    else if (first_unit == "cm" && second_unit == "mm")
+    {
+        result = myInput * 10;
+    }
+
+    else if (first_unit == "cm" && second_unit == "cm")
+    {
+        result = myInput;
+    }
+
+    else if (first_unit == "cm" && second_unit == "m")
+    {
+        result = myInput / 100;
+    }
+
+    else if (first_unit == "cm" && second_unit == "km")
+    {
+        result = myInput / 1000;
+    }
+
+    // METER CONVERSIONS
+    else if (first_unit == "m" && second_unit == "mm")
+    {
+        result = myInput * 1000;
+    }
+
+    else if (first_unit == "m" && second_unit == "cm")
+    {
+        result = myInput * 100;
+    }
+
+    else if (first_unit == "m" && second_unit == "m")
+    {
+        result = myInput;
+    }
+
+    else if (first_unit == "m" && second_unit == "km")
+    {
+        result = myInput / 1000 ;
+    }
+
+    // KIOLOMETER CONVERSIONS
+    else if (first_unit == "km" && second_unit == "mm")
+    {
+        result = myInput * 1000000;
+    }
+
+    else if (first_unit == "km" && second_unit == "cm")
+    {
+        result = myInput * 10000;
+    }
+    
+    else if (first_unit == "km" && second_unit == "m")
+    {
+        result = myInput * 1000;
+    }
+
+    else if (first_unit == "km" && second_unit == "km")
+    {
+        result = myInput;
+    }
+
+    document.getElementById("convertOut").innerHTML = result + " " + second_unit;
+ }
